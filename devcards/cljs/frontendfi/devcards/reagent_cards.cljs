a2b8efe5f46b832ad5fc9e3656b12c5facc01f32
Teemu Kaukoranta <teemu.kaukoranta@solita.fi>
2016-05-24 12:54:10 +0300

    More stuff to reagent demo

--- devcards/cljs/frontendfi/devcards/reagent_cards.cljs
+++ devcards/cljs/frontendfi/devcards/reagent_cards.cljs
@@ -9,25 +9,16 @@
 
 (defcard-rg People-list
             (fn [state _] [app/people-list state])
-            (r/atom [{:name "Bob" :age 50} {:name "Lisa" :age 32}]))
-
-(defn project-status [departments people]
-  (let [employed-count (reduce + (map (comp count :people second) @departments))
-        people-count (count @people)
-        jobless (- people-count employed-count)]
-    (cond
-      (> jobless 1) [:div.jobless-warning "Too many people without projects"]
-      (= jobless 0) [:div.no-free-people "Lack of human resources"]
-      :else [:div.good-project-status "Everything is awesome"])))
+            (r/atom {0 {:name "Bob" :age 50} 1 {:name "Lisa" :age 32}}))
 
 (defcard-rg Lack-of-resources
-            (fn [state _ ] [project-status
+            (fn [state _ ] [app/project-status
                             (r/cursor state model/department-path)
                             (r/cursor state model/people-path)])
             (r/atom model/initial))
 
 (defcard-rg Too-many-people
-            (fn [state _ ] [project-status
+            (fn [state _ ] [app/project-status
                             (r/cursor state model/department-path)
                             (r/cursor state model/people-path)])
             (r/atom {:people/by-id {0 {:name "Mikko", :age 19, :gender "Male", :id 0},
@@ -37,7 +28,7 @@
             {:inspect-data false})
 
 (defcard-rg Everything-is-awesome
-            (fn [state _ ] [project-status
+            (fn [state _ ] [app/project-status
                             (r/cursor state model/department-path)
                             (r/cursor state model/people-path)])
             (r/atom {:people/by-id {0 {:name "Mikko", :age 19, :gender "Male", :id 0},
--- src/cljs/frontendfi/reagent/app.cljs
+++ src/cljs/frontendfi/reagent/app.cljs
@@ -3,20 +3,47 @@
             [ajax.core :refer [GET POST]]
             [frontendfi.models.model :as model]))
 
+(defn project-status [departments people]
+  (let [employed-count (reduce + (map (comp count :people second) @departments))
+        people-count (count @people)
+        jobless (- people-count employed-count)]
+    (cond
+      (> jobless 1) [:div.jobless-warning "Too many people without projects"]
+      (= jobless 0) [:div.no-free-people "Lack of human resources"]
+      :else [:div.good-project-status "Everything is awesome"])))
 
 (defn people-list [people-atom]
   [:ul
    (map-indexed
-     (fn [i p]
+     (fn [i [_ p]]
        [:li
         {:class (if (even? i) "even" "uneven")
-         :key (str "person_in_list_" (:name p))}
+         :key (str "person_in_list_" i)}
         (str (:name p) ", " (:age p))])
      @people-atom)])
 
+(defn department [dept employees]
+  [:div
+   (:name dept) " has " (count employees) " employees"
+   [:ul
+    (map
+      (fn [emp]
+        [:li
+         {:key (str "emp_in_list_" (:name emp) (:age emp) (:name dept))}
+         (str (:name emp) ", " (:age emp))])
+      employees)]])
+
+(defn departments [departments people]
+  [:div
+   (for [[_ d] @departments]
+     ^{:key (str "department_" (:name d))}
+     [department d (map (fn [emp-q] (get-in @people (rest emp-q))) (:people d))])])
+
 (defn app [atom]
   [:div "Reagent application"
-   [people-list (r/cursor atom model/people-path)]])
+   [project-status (r/cursor atom model/department-path) (r/cursor atom model/people-path)]
+   [people-list (r/cursor atom model/people-path)]
+   [departments (r/cursor atom model/department-path) (r/cursor atom model/people-path)]])
 
 (defn mount! [element]
   (r/render-component [app model/model] element))
